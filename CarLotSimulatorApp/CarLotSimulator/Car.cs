86e92edd0f588a6ca3302144212d292ff64373fd
Tiffanygray2383 <63433432+Tiffanygray2383@users.noreply.github.com>
2021-03-23 23:46:31 -0400

    Car Lot Simulator assignment

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car                   //*******I can not access or call this function properly using the Console.WriteLine() - Can you assist me with this is out tutoring session?- Thank you ! 
+    {
+        public string MakeEngineNoise = "Vurm Verm";
+
+        public int Year = 2021;
+        public string model = "Accord";
+        public string make = "Honda";
+        public string EngineNoise = "Loud";
+        public string HonkNoise = "Chirp";
+        public bool IsDriveable = true;
+
+        //Vehicle 2
+        public string MakeEngineNoise2 = "Vurm Verm";
+
+        public int Year2 = 2021;
+        public string model2 = "Camero";
+        public string make2 = "Chevy";
+        public string EngineNoise2 = "Loud";
+        public string HonkNoise2 = "Chirp";
+        public bool IsDriveable2 = true;
+
+
+        //Vehicle 3
+        public string MakeEngineNoise3 = "Vurm Verm";
+
+        public int Year3 = 2025;
+        public string model3 = "Escalade";
+        public string make3 = "Cadeillac";
+        public string EngineNoise3 = "Loud";
+        public string HonkNoise3 = "Chirp";
+        public bool IsDriveable3 = true;
+
+        //Vehicle 4
+        public string MakeEngineNoise4 = "Vurm Verm";
+
+        public int Year4 = 2021;
+        public string model4 = "TownCar";
+        public string make4 = "Lincoln";
+        public string EngineNoise4 = "Loud";
+        public string HonkNoise4 = "Chirp";
+        public bool IsDriveable4 = true;
+        //var MakeHonkNoise();
+    }
+
+
+}
